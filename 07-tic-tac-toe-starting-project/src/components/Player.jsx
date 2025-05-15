c25843bea966484a18a108207d4c694963481369
Hwasser <victorhwasser@gmail.com>
2025-05-15 09:13:04 +0200

    section 4: 81

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = 'Edit';
+
+  function handleEditClick() {
+    // setIsEditing(!isEditing);
+    // Det här är ett dåligt sätt att utföra det på, i react
+    // bör man istället skicka en funktion.
+    setIsEditing(prev => !prev);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  if (isEditing) {
+    editablePlayerName = <input type='text' value={playerName} onChange={handleChange} required />
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  )
+}
