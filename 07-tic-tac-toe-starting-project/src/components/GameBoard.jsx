51cf3c7cbad2f3d8a3da7ecd43be8d0f831cb38d
Hwasser <victorhwasser@gmail.com>
2025-05-15 09:36:25 +0200

    section 4: 82 - added player turns

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const initialGameBoard = [
     [null, null, null],
@@ -6,15 +6,27 @@ const initialGameBoard = [
     [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	function handleSelectSquare(rowIndex, colIndex) {
+		setGameBoard(prevBoard => {
+			const updatedBoard = [...prevBoard].map(innerArray => [...innerArray]); 
+			updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+			return updatedBoard
+		});
+
+		onSelectSquare();
+	}
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
 						{row.map((playerSymbol, colIndex) => 
 							<li key={colIndex}>
-								<button>{playerSymbol}</button>
+								<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
 							</li>
 						)}
 					</ol>
