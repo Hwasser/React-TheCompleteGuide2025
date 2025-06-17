74bdd22ce2f39db125fcc3bfb67090dbc3c10dd8
Hwasser <victorhwasser@gmail.com>
2025-06-17 07:34:08 +0200

    section8: 144 completed

--- section8/src/components/TimerChallenge.jsx
+++ section8/src/components/TimerChallenge.jsx
@@ -5,28 +5,45 @@ export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
   
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
+  const timeStep = 10;
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-  function handleStart() {
-    setTimerStarted(true);
-    timer.current = setTimeout(() => { 
-      setTimerExpired(true) 
-      // dialog.current.showModal(); - tidigare lösning. Men vi har en custom open-metod istället
-      // för att man inte skall vara beroende av att kalla metoder direkt i source coden
-      // -> Bättre de-coupling mellan komponenter.
-      dialog.current.open();
-    }, targetTime * 1000);
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
   }
 
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => { 
+      setTimeRemaining(prev => prev - timeStep);
+    }, timeStep);
+  }
+
+  // function handleStart() {
+  //   setTimerStarted(true);
+  //   timer.current = setTimeout(() => { 
+  //     setTimerExpired(true) 
+  //     // dialog.current.showModal(); - tidigare lösning. Men vi har en custom open-metod istället
+  //     // för att man inte skall vara beroende av att kalla metoder direkt i source coden
+  //     // -> Bättre de-coupling mellan komponenter.
+  //     dialog.current.open();
+  //   }, targetTime * 1000);
+  // }
+
   function handleStop() {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialog.current.open();
   }
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result={"lost"} />     
+      <ResultModal ref={dialog} targetTime={targetTime} remainingTime={timeRemaining} onReset={handleReset} />     
       <section className="challenge">
         TimerChallenge
         <h2>{title}</h2>
@@ -34,12 +51,12 @@ export default function TimerChallenge({ title, targetTime }) {
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? 'Stop' : 'Start'} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? 'Stop' : 'Start'} Challenge
           </button>
         </p>
-        <p className={timerStarted ? 'active' : ''}>
-          {timerStarted ? 'Timer is running' : 'Timer inactive' }
+        <p className={timerIsActive ? 'active' : ''}>
+          {timerIsActive ? 'Timer is running' : 'Timer inactive' }
         </p>
       </section>
     </>
