dc73166d6d7fdaf13fa7cea6d0f75b14bf561c0b
Hwasser <victorhwasser@gmail.com>
2025-09-08 12:49:21 +0200

    Section 12: 195 completed

---
+++ section12/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../assets/quiz-logo.png';
+
+export default function Header() {
+
+
+  return (
+    <header>
+      <img src={Logo} alt='Quiz logo' />
+      <h1>ReactQuiz</h1>
+    </header>
+  )
+}
